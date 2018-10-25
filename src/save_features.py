41e5e676bd8b28e34db01f614a7fe14b72985a90
luke <l.liciniuslucullus@gmail.com>
2018-10-25 21:11:32 +0200

    prepared save_features to genearte nontest dataset

--- src/save_features.py
+++ src/save_features.py
@@ -23,9 +23,11 @@ def main():
     #        features_funcs.dists_eyebrows,
     #        features_funcs.dists_eyebrows_corners,
     #        features_funcs.dists_eyes]
-    ffs = [features_funcs.butterfly_catastrophe]
+    ffs = [features_funcs.butterfly_catastrophe, features_funcs.ellipse_picked,
+           features_funcs.ellipse, features_funcs.polyfit6, features_funcs.butterfly_catastrophe,
+           features_funcs.face_contour]
 
-    test_data = True
+    test_data = False
 
     for feature_func in ffs:
         print('get features (with gender)', flush=True)
